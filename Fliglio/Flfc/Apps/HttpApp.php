c9c6578f491a1ac21786f620fc0978afc94b7732
Ben Schwartz <benschw@gmail.com>
2015-02-25 12:19:27 -0600

    refactor(rawResponse): setting on Context rather than returning from wrappedApp

--- Fliglio/Flfc/Apps/HttpApp.php
+++ Fliglio/Flfc/Apps/HttpApp.php
@@ -15,20 +15,26 @@ class HttpApp extends MiddleWare {
 		/* Call Child App
 		 */
 		try {
-			$to = $this->wrappedApp->call($context);
-			if (is_object($to)) {
-				$reflector = new \ReflectionClass(get_class($to));
-				if ($reflector->implementsInterface("Fliglio\Http\ResponseBody")) {
-					$context->getResponse()->setBody($to);
-				}
-			} 
-
+			$this->wrappedApp->call($context);
 		} catch (RedirectException $e) {
 			$context->getResponse()->addHeader("Location", (string)$e->getLocation());
 			$context->getResponse()->setStatus($e->getStatusCode());
 		}
 
 		$response = $context->getResponse();
+
+		if ($context->isPropSet('rawResponse')) {
+			$to = $context->getProp('rawResponse');
+
+			if (is_object($to)) {
+				$reflector = new \ReflectionClass(get_class($to));
+				if ($reflector->implementsInterface("Fliglio\Http\ResponseBody")) {
+					$response->setBody($to);
+					$context->unsetProp('rawResponse');
+				}
+			} 
+		}
+
 
 		if (is_null($response->getStatus())) {
 			$response->setStatus(200);
--- Fliglio/Flfc/Apps/RestApp.php
+++ Fliglio/Flfc/Apps/RestApp.php
@@ -10,37 +10,45 @@ use Fliglio\Flfc\Exceptions\BadRequestException;
 class RestApp extends MiddleWare {
 	
 	public function call(Context $context) {
-		$resp = $context->getResponse();
+		$response = $context->getResponse();
 
-		$content = null;
 		try {
-			$to = $this->wrappedApp->call($context);
+			$this->wrappedApp->call($context);
+
+		} catch (PageNotFoundException $e) {
+			$response->setStatus(404);
+			$response->setContentType('application/json');
+			return;
+		} catch (BadRequestException $e) {
+			error_log((string)$e);
+			$response->setStatus(500);
+			$response->setContentType('application/json');
+			return;
+		}	
+
+		// if rawResponse is a ResponseBody, use it
+		if ($context->isPropSet('rawResponse')) {
+			$to = $context->getProp('rawResponse');
 
 			if (is_object($to)) {
 				$reflector = new \ReflectionClass(get_class($to));
 				if ($reflector->implementsInterface("Fliglio\Http\ResponseBody")) {
-					$content = $to;
+					$response->setBody($to);
+					$context->unsetProp('rawResponse');
 				}
 			} 
+		}
 
-			if (is_null($content)) {
-				$json = is_null($to) ? '' : json_encode($to);
-				$content = new DefaultView($json);
-			}
-		} catch (PageNotFoundException $e) {
-			$resp->setStatus(404);
-			$resp->addHeader('Content-Type', 'application/json');
-		} catch (BadRequestException $e) {
-			error_log((string)$e);
-			$resp->setStatus(500);
-			$resp->addHeader('Content-Type', 'application/json');
-		}	
+		// if rawResponse is unknown type, json_encode it
+		if ($context->isPropSet('rawResponse')) {
+			$to = $context->getProp('rawResponse');
+			$json = is_null($to) ? '' : json_encode($to);
+			$response->setBody(new DefaultView($json));
+			$context->unsetProp('rawResponse');
+		}
 
-
-		$resp->setBody($content);
-
-		if (is_null($resp->getContentType())) {
-			$resp->setContentType('application/json');
+		if (is_null($response->getContentType())) {
+			$response->setContentType('application/json');
 		}
 	}
 
